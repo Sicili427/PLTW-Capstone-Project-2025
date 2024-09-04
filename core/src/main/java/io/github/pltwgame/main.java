3d92076e54db2223da18440d3db6d0adaff18e69
licon <li.conner622@gmail.com>
2024-09-04 09:16:03 -0400

    LOL

--- core/src/main/java/io/github/pltwgame/main.java
+++ core/src/main/java/io/github/pltwgame/main.java
@@ -1,11 +1,12 @@
 package io.github.pltwgame;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
 
@@ -13,12 +14,12 @@ public class Main extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
-        System.out.print("help");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
