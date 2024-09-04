4a9e785389b209a8d26791e9e2fd5d272ecaae6c
persam0759 <samuelpersinger@icloud.com>
2024-09-04 13:10:05 +0000

    Change

--- core/src/main/java/io/github/pltwgame/Main.java
+++ core/src/main/java/io/github/pltwgame/Main.java
@@ -1,8 +1,5 @@
 package io.github.pltwgame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -16,6 +13,7 @@ public class Main extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        System.out.print("help");
     }
 
     @Override
