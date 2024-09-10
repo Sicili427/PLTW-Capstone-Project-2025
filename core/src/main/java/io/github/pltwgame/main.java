dc0ece4cf44b79ece6d2621daf66d3cc727f29f3
licon <li.conner622@gmail.com>
2024-09-10 08:35:07 -0400

    Stuff

--- core/src/main/java/io/github/pltwgame/main.java
+++ core/src/main/java/io/github/pltwgame/main.java
@@ -9,20 +9,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link ApplicationListener} implementation shared by all platforms. */
 public class main extends ApplicationAdapter {
-    private Viewport viewport;
-    private SpriteBatch batch;
-    private Texture line;
-    private ShapeRenderer shapeRenderer;
+    final float SCREEN_WIDTH = 1280;
+    final float SCREEN_HEIGHT = Math.round((9 * SCREEN_WIDTH) / 16);
+
+    final float GRID_WIDTH = 128;
+    final float GRID_HEIGHT = 64;
+
+    Viewport viewport;
+
+
+    SpriteBatch batch;
+    Texture line;
+    ShapeRenderer shapeRenderer;
 
 
     @Override
     public void create() {
-        viewport = new FillViewport(1600, 900);
+        viewport = new FitViewport(SCREEN_WIDTH, SCREEN_HEIGHT);
         shapeRenderer = new ShapeRenderer();
         batch = new SpriteBatch();
         line = new Texture("pixel.png");
@@ -35,8 +43,6 @@ public class main extends ApplicationAdapter {
 
     @Override
     public void render() {
-        // Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
-
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
 
         renderLine();
@@ -59,7 +65,7 @@ public class main extends ApplicationAdapter {
 
         batch.begin();
         for(int i = 0; i < resolution; i++) {
-            batch.draw(line, (float) (i * (1600/resolution)), (float) (20*Math.sin(i) + 200));
+            batch.draw(line, (float) (i * (1600/resolution)), (float) (10*Math.sqrt(i) + 200));
         }
         batch.end();
     }
