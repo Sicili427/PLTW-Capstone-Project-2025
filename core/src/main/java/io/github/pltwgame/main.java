e828d2a01139b1151d58afe7693f54c94bf8243d
coxjac0615 <coxjac0615@ccsdschools.com>
2024-09-11 09:47:37 -0400

    Made the circle move

--- core/src/main/java/io/github/pltwgame/main.java
+++ core/src/main/java/io/github/pltwgame/main.java
@@ -3,6 +3,7 @@ package io.github.pltwgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -19,13 +20,15 @@ public class main extends ApplicationAdapter {
 
     final float GRID_WIDTH = 128;
     final float GRID_HEIGHT = 64;
-
+    float circleX = 100;
+    float circleY = 100;
     Viewport viewport;
 
     SpriteBatch batch;
     Texture line;
     ShapeRenderer shapeRenderer;
 
+    MyInputProcessor inputProcessor = new MyInputProcessor();
 
     @Override
     public void create() {
@@ -45,11 +48,28 @@ public class main extends ApplicationAdapter {
         ScreenUtils.clear(1f, 1f, 1f, 1f);
 
         renderLine();
-
+        drawBoard();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(Color.RED);
         shapeRenderer.line(200, 0, 1000, 900);
         shapeRenderer.end();
+        if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            circleX+=SCREEN_WIDTH/320;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            circleX-=SCREEN_WIDTH/320;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.S)){
+            circleY-=SCREEN_HEIGHT/320;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.W)){
+            circleY+=SCREEN_HEIGHT/320;
+        }
+            circleX = Gdx.input.getX();
+            circleY = SCREEN_HEIGHT-Gdx.input.getY();
+            if(Gdx.input.isKeyPressed(Input.Keys.W)){
+                Gdx.input.setCursorPosition(100, 100);
+            }
     }
 
     @Override
@@ -60,7 +80,10 @@ public class main extends ApplicationAdapter {
     }
 
     private void drawBoard() {
-
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.BROWN);
+        shapeRenderer.circle(circleX, circleY, 50);
+        shapeRenderer.end();
     }
 
     private void renderLine() {
@@ -73,4 +96,6 @@ public class main extends ApplicationAdapter {
         }
         shapeRenderer.end();
     }
+
 }
+
