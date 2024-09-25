100b2e817834d0681eb813dea71cc83e6d825515
licon <li.conner622@gmail.com>
2024-09-25 09:55:49 -0400

    Stuff

--- core/src/main/java/io/github/pltwgame/main.java
+++ core/src/main/java/io/github/pltwgame/main.java
@@ -24,9 +24,6 @@ public class main extends ApplicationAdapter {
     float circleX = 100;
     float circleY = 100;
 
-    final float xSpot = (float) Math.random()*SCREEN_WIDTH;
-    final float ySpot = (float) Math.random()*SCREEN_HEIGHT;
-
     Grid grid;
 
     Stage stage;
@@ -37,10 +34,7 @@ public class main extends ApplicationAdapter {
     OrthographicCamera camera;
 
     SpriteBatch batch;
-    Texture line;
     ShapeRenderer shapeRenderer;
-
-    MyInputProcessor inputProcessor = new MyInputProcessor();
 
     @Override
     public void create() {
@@ -49,10 +43,10 @@ public class main extends ApplicationAdapter {
         viewport.apply();
         camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
         stage = new Stage(viewport);
-        grid = new Grid(SCREEN_WIDTH, SCREEN_HEIGHT,64,2,1);
+        grid = new Grid(SCREEN_WIDTH,(SCREEN_HEIGHT),64,2,1);
+        grid.offsetY = (int)(SCREEN_HEIGHT*0.1);
 
         shapeRenderer = new ShapeRenderer();
-        batch = new SpriteBatch();
 
         Gdx.app.setLogLevel(Application.LOG_INFO);
     }
@@ -68,21 +62,19 @@ public class main extends ApplicationAdapter {
         camera.update();
         drawBoard();
 
-        generateLine(grid);
-        circleX = Gdx.input.getX();
+        grid.generateLine();
+        /* circleX = Gdx.input.getX();
         circleY = SCREEN_HEIGHT-Gdx.input.getY();
         testAI.moveToPoint();
         if(Gdx.input.isKeyPressed(Input.Keys.S)) {
             float xSpot = (float) Math.random()*SCREEN_WIDTH;
             float ySpot = (float) Math.random()*SCREEN_HEIGHT;
             testAI.addPoint(xSpot, ySpot, false);
-            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-            shapeRenderer.setColor(Color.BROWN);
-            shapeRenderer.circle(xSpot, ySpot, 50);
-            shapeRenderer.end();
         }
         testAI.drawAI(shapeRenderer);
         testAI.moveToPoint(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+         */
     }
 
     @Override
@@ -94,48 +86,6 @@ public class main extends ApplicationAdapter {
 
     private void drawBoard() {
         grid.generateGrid();
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(Color.BROWN);
-        shapeRenderer.circle(circleX, circleY, 50);
-        shapeRenderer.end();
-    }
-
-    private void generateLine(Grid initGrid) {
-        // finds and puts points in an array
-        int resolution = 1000;
-        int size = grid.numVertLines * 1000;
-        Vector2[] points = new Vector2[size];
-
-        int offsetX = 0;
-        int offsetY = initGrid.horzLines.length/2;
-
-        for(int i = 0; i < size; i++) {
-            double input = i / (double) resolution;
-            float y = (float) (Math.tan(input));
-            points[i] = new Vector2((float) input, y);
-        }
-
-        for(int i = 0; i < points.length; i++) {
-            if (!Float.isInfinite(points[i].y) || !Float.isNaN(points[i].y)) {
-                if (Math.floor(points[i].y) > initGrid.horzLines.length - offsetY || Math.floor(points[i + 1].y) > initGrid.horzLines.length - offsetY) {
-                    continue;
-                } else if (Math.floor(Math.abs(points[i].y)) > offsetY || Math.floor(Math.abs(points[i + 1].y)) > offsetY) {
-                    continue;
-                }
-
-                float x1 = initGrid.vertLines[(int) points[i].x + offsetX].x + (points[i].x - (int) points[i].x) * initGrid.CellX;
-                float y1 = initGrid.horzLines[(int) points[i].y + offsetY].y + (points[i].y - (int) points[i].y) * initGrid.CellY;
-
-                float x2 = initGrid.vertLines[(int) points[i + 1].x + offsetX].x + (points[i + 1].x - (int) points[i + 1].x) * initGrid.CellX;
-                float y2 = initGrid.horzLines[(int) points[i + 1].y + offsetY].y + (points[i + 1].y - (int) points[i + 1].y) * initGrid.CellY;
-
-                shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-                shapeRenderer.setColor(Color.RED);
-                shapeRenderer.line(x1, y1, x2, y2);
-                shapeRenderer.end();
-            }
-        }
     }
 
 }
