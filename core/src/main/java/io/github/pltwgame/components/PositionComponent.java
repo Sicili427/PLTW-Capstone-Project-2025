e405bc5c853c7aad738c942fe45459b3c76aa4e4
coxjac0615 <coxjac0615@ccsdschools.com>
2025-04-22 08:49:01 -0400

    h

--- core/src/main/java/io/github/pltwgame/components/PositionComponent.java
+++
@@ -1,8 +0,0 @@
-package io.github.pltwgame.components;
-
-import com.artemis.Component;
-
-public class PositionComponent extends Component{
-    public float x, y;
-    public float angle;
-}
--- core/src/main/java/io/github/pltwgame/components/WanderComponent.java
+++
@@ -1,6 +0,0 @@
-package io.github.pltwgame.components;
-
-import com.artemis.Component;
-
-public class WanderComponent extends Component {
-}
